cb16a4329cef997c45324c9b781f157d5899f159
Dani Shulman <mail@webcore1.com>
2018-04-23 23:02:04 -0400

    Gallery with Ant Row/Col and responsive for mobile.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,118 +1,160 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { Carousel, Icon } from 'antd';
-import 'antd/lib/carousel/style/css';
-import 'antd/lib/icon/style/css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Carousel, Icon } from "antd";
+import "antd/lib/carousel/style/css";
+import "antd/lib/icon/style/css";
 
 class Gallery extends Component {
+  render() {
+    const { images, onCarouselChange, activeImage } = this.props;
+    const Container = styled.div`
+      width: 100%;
+      height: 80vh;
+      margin-top: 10vh;
+      position: relative;
+    `;
+    const ArrowContainer = styled.div`
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 80%;
+      height: 100%;
+      margin-left: 10%;
+      z-index: 3;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    `;
+    const Arrow = styled.span`
+      font-size: 4vmin;
+      color: rgba(42, 17, 13, 0.7);
+      &:hover {
+        cursor: pointer;
+        color: rgba(42, 17, 13, 1);
+        font-size: 4.2vmin;
+        transition: all 0.5s;
+      }
+    `;
 
-	render() {
-		const { images, onCarouselChange, activeImage } = this.props;
-		const Container = styled.div`
-			width: 80%;
-			height: 80vh;
-			margin-top: 10vh;
-			position: relative;
-		`;
-		const ArrowContainer = styled.div`
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 80%;
-			height: 100%;
-			margin-left: 10%; 
-			z-index: 3;
-			display: flex;
-			flex-direction: row;
-			justify-content: space-between;
-			align-items: center;
-		`;
-		const Arrow = styled.span`
-			font-size: 4vmin;
-			color: rgba(42, 17, 13, 0.7);
-			&:hover {
-				cursor: pointer;
-				color: rgba(42, 17, 13, 1);
-				font-size: 4.2vmin;
-				transition: all 0.5s;
-			};
-		`
+    const onChange = ix => {
+      onCarouselChange(images[ix].id, ix);
+    };
 
-		const onChange = (ix) => {
-			onCarouselChange(images[ix].id, ix);
-		}
+    const Image = styled.img`
+      height: 80vh;
+      width: auto;
+      margin: 0 auto;
+    `;
+    const Thumbnail = styled.img`
+      width: 40px;
+      height: auto;
+      max-height: 45px;
+      &:hover {
+        width: 45px;
+        height: auto;
+        max-height: 50px;
+        cursor: pointer;
+        transition: all 0.4s ease-out;
+      }
+    `;
+    const ActiveThumbnail = styled.img`
+      width: 45px;
+      height: auto;
+      max-height: 45px;
+      &:hover {
+        cursor: pointer;
+      }
+    `;
 
-		const Image = styled.img`
-			height: 80vh;
-			width: auto;
-			margin: 0 auto;
-		`;
-		const Thumbnail = styled.img`
-			width: 40px;
-			height: auto;
-			max-height: 45px;
-			&:hover {
-				width: 45px;
-				height: auto;
-				max-height: 50px;
-				cursor: pointer;
-				transition: all .4s ease-out;
-			}
-		`;
-		const ActiveThumbnail = styled.img`
-			width: 45px;
-			height: auto;
-			max-height: 45px;
-			&:hover {
-				cursor: pointer;
-			};
-		`
+    const renderCarouselItems = () => {
+      return images.map(i => (
+        <div key={i.id}>
+          <Image
+            className="carousel-image"
+            id={i.id}
+            alt={i.categories[0]}
+            key={i.id}
+            src={i.path}
+          />
+        </div>
+      ));
+    };
 
-		const renderCarouselItems = () => {
-			return images.map(i => <div key={i.id}><Image className="carousel-image" id={i.id} alt={i.categories[0]} key={i.id} src={i.path} /></div>);
-		}
+    const renderCarouselThumbnails = dots => {
+      return (
+        <ul>
+          {images.map((img, ix) => {
+            return (
+              <span
+                key={img.id}
+                id={img.id}
+                onClick={e => {
+                  this.refs.carousel.goTo(
+                    images.findIndex(i => i.id == e.target.id)
+                  );
+                }}
+              >
+                {ix === activeImage ? (
+                  <ActiveThumbnail
+                    id={img.id}
+                    alt={img.categories[0]}
+                    src={img.path}
+                  />
+                ) : (
+                  <Thumbnail
+                    id={img.id}
+                    alt={img.categories[0]}
+                    src={img.path}
+                  />
+                )}
+              </span>
+            );
+          })}
+        </ul>
+      );
+    };
 
-		const renderCarouselThumbnails = (dots) => {
-			return <ul>{images.map((img, ix) => {
-				return (
-					<span key={img.id} id={img.id} onClick={(e) => { this.refs.carousel.goTo(images.findIndex(i=> i.id==e.target.id)); }}>
-						{ix === activeImage ?
-							<ActiveThumbnail id={img.id} alt={img.categories[0]} src={img.path} /> :
-							<Thumbnail id={img.id} alt={img.categories[0]} src={img.path} />
-						}
-					</span>
-				);
-			})}</ul>;
-		}
+    const renderCarouselThumbnails1 = () => {
+      return (
+        <ul>
+          {images.map(i => {
+            <div
+              key={i.id}
+              id={i.id}
+              onClick={e => onChange(images.indexOf(e.target.id))}
+            >
+              <Thumbnail alt={i.categories[0]} src={i.path} />
+            </div>;
+          })}
+        </ul>
+      );
+    };
 
-		const renderCarouselThumbnails1 = () => {
-			return <ul>{images.map(i => {
-					<div key={i.id} id={i.id} onClick={(e) => onChange(images.indexOf(e.target.id))}>
-						<Thumbnail alt={i.categories[0]} src={i.path} />
-					</div>
-			})}</ul>
-		}
-
-		return (
-			<Container>
-				<ArrowContainer>
-					<Arrow onClick={() => this.refs.carousel.prev()}><Icon type="double-left" /></Arrow>
-					<Arrow onClick={() => this.refs.carousel.next()}><Icon type="double-right" /></Arrow>
-				</ArrowContainer>
-				<Carousel
-					ref="carousel"
-					initialSlide={activeImage}
-					afterChange={onChange}
-					autoplay
-					draggable
-					infinite={false}
-					appendDots={renderCarouselThumbnails}
-				>
-					{renderCarouselItems()}
-				</Carousel>
-			</Container>
-		);
-	};
+    return (
+      <Container>
+        <ArrowContainer>
+          <Arrow onClick={() => this.refs.carousel.prev()}>
+            <Icon type="double-left" />
+          </Arrow>
+          <Arrow onClick={() => this.refs.carousel.next()}>
+            <Icon type="double-right" />
+          </Arrow>
+        </ArrowContainer>
+        <Carousel
+          ref="carousel"
+          initialSlide={activeImage}
+          afterChange={onChange}
+          autoplay
+          draggable
+          infinite={false}
+          appendDots={renderCarouselThumbnails}
+        >
+          {renderCarouselItems()}
+        </Carousel>
+      </Container>
+    );
+  }
 }
 
 export default Gallery;
--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,118 +1,130 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-import Gallery from './Gallery';
-import TagList from './TagList';
+import Gallery from "./Gallery";
+import TagList from "./TagList";
+
+import { Row, Col } from "antd";
+import "antd/lib/grid/style/css";
 
 class GallerySection extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			activeImage: {
-				id: 0,
-				ix: 0,
-			},
-			selectedTags: ['All'],
-			filteredImages: props.images,
-			tags: props.images
-				/* Map to list of arrays: [['a', 'b', 'c'], ['b', 'd'], [...]] */
-				.map(i => i.categories)
-				/* Combine arrays into one array: ['a', 'b', 'c', 'b', 'd'] */
-				.reduce((i, acc) => [...acc, ...i])
-				/* Sort array: ['a', 'b', 'b', 'c', 'd'] */
-				.sort()
-				/* Remove duplicates: ['a', 'b', 'c', 'd'] */
-				.filter((v, i, a) => (i===0 || v!==a[i-1])),
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeImage: {
+        id: 0,
+        ix: 0
+      },
+      selectedTags: ["All"],
+      filteredImages: props.images,
+      tags: props.images
+        /* Map to list of arrays: [['a', 'b', 'c'], ['b', 'd'], [...]] */
+        .map(i => i.categories)
+        /* Combine arrays into one array: ['a', 'b', 'c', 'b', 'd'] */
+        .reduce((i, acc) => [...acc, ...i])
+        /* Sort array: ['a', 'b', 'b', 'c', 'd'] */
+        .sort()
+        /* Remove duplicates: ['a', 'b', 'c', 'd'] */
+        .filter((v, i, a) => i === 0 || v !== a[i - 1])
+    };
+  }
 
-	filterImages = (tags) => {
-		if (tags[0] === 'All') {
-			return this.props.images;
-		}
-		return this.props.images.filter(img => {
-			for (var i=0; i<tags.length; i++) {
-				if (img.categories.includes(tags[i])) {
-					return true;
-				}
-			}
-			return false;
-		});
-	};
+  filterImages = tags => {
+    if (tags[0] === "All") {
+      return this.props.images;
+    }
+    return this.props.images.filter(img => {
+      for (var i = 0; i < tags.length; i++) {
+        if (img.categories.includes(tags[i])) {
+          return true;
+        }
+      }
+      return false;
+    });
+  };
 
-	selectTag = (tag) => {
-		let newTags;
-		if (tag === 'All') {
-			newTags = ['All'];
-		} else if (this.state.selectedTags[0] === 'All') {
-			newTags = [tag];
-		} else {
-			newTags = [tag, ...this.state.selectedTags];
-		}
-		console.log("Set tags to ", newTags);
-		let newImgs = this.filterImages(newTags);
-		console.log("Set images to ", newImgs);
-		this.setState({
-			selectedTags: newTags,
-			activeImage: { id: newImgs[0].id, ix: 0 },
-			filteredImages: newImgs
-		});
-	};
+  selectTag = tag => {
+    let newTags;
+    if (tag === "All") {
+      newTags = ["All"];
+    } else if (this.state.selectedTags[0] === "All") {
+      newTags = [tag];
+    } else {
+      newTags = [tag, ...this.state.selectedTags];
+    }
+    console.log("Set tags to ", newTags);
+    let newImgs = this.filterImages(newTags);
+    console.log("Set images to ", newImgs);
+    this.setState({
+      selectedTags: newTags,
+      activeImage: { id: newImgs[0].id, ix: 0 },
+      filteredImages: newImgs
+    });
+  };
 
-	unselectTag = (tag) => {
-		let newTags;
-		if (this.state.selectedTags.length === 1) {
-			newTags = ['All'];
-		} else {
-			let ix = this.state.selectedTags.indexOf(tag);
-			newTags = [...this.state.selectedTags.slice(0, ix), ...this.state.selectedTags.slice(ix+1)];
-		}
-		let newImgs = this.filterImages(newTags);
-		this.setState({
-			selectedTags: newTags,
-			activeImage: { id: newImgs[0].id, ix: 0 },
-			filteredImages: newImgs
-		});
-	};
+  unselectTag = tag => {
+    let newTags;
+    if (this.state.selectedTags.length === 1) {
+      newTags = ["All"];
+    } else {
+      let ix = this.state.selectedTags.indexOf(tag);
+      newTags = [
+        ...this.state.selectedTags.slice(0, ix),
+        ...this.state.selectedTags.slice(ix + 1)
+      ];
+    }
+    let newImgs = this.filterImages(newTags);
+    this.setState({
+      selectedTags: newTags,
+      activeImage: { id: newImgs[0].id, ix: 0 },
+      filteredImages: newImgs
+    });
+  };
 
-	onChange = (id, ix) => {
-		this.setState({ activeImage: { id, ix }});
-	}
+  onChange = (id, ix) => {
+    this.setState({ activeImage: { id, ix } });
+  };
 
-	render() {
-		const { images } = this.props;
-		const Container = styled.div`
-			width: 100%;
-			height: 80vh;
-			z-index: -2;
-			display: flex;
-			flex-direction: row;
-			margin-top: 20px;
-		`;
-		const Details = styled.div`
-			width: 18%;
-			height: 80vh;
-			display: flex;
-			flex-direction: column;
-			justify-content: space-between;
-			margin: 10vh 2.5% 0 2.5%;
-		`;
+  render() {
+    const { images } = this.props;
+    /* const Container = styled.div`
+      width: 100%;
+      height: 80vh;
+      z-index: -2;
+      display: flex;
+      flex-direction: row;
+      margin-top: 20px;
+    `;
+    const Details = styled.div`
+      width: 18%;
+      height: 80vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      margin: 10vh 2.5% 0 2.5%;
+    `;*/
 
-		return (
-			<Container>
-				<Gallery images={this.state.filteredImages} onCarouselChange={this.onChange} activeImage={this.state.activeImage.ix} />
-				<Details>
-					<TagList
-						tags={this.state.tags}
-						activeTags={images[this.state.activeImage.id].categories}
-						selectedTags={this.state.selectedTags}
-						selectTag={this.selectTag}
-						unselectTag={this.unselectTag}
-					/>
-				</Details>
-			</Container>
-		);
-	}
+    return (
+      <Row>
+        <Col md={20} xs={24}>
+          <Gallery
+            images={this.state.filteredImages}
+            onCarouselChange={this.onChange}
+            activeImage={this.state.activeImage.ix}
+          />
+        </Col>
+        <Col md={4} xs={24}>
+          <TagList
+            tags={this.state.tags}
+            activeTags={images[this.state.activeImage.id].categories}
+            selectedTags={this.state.selectedTags}
+            selectTag={this.selectTag}
+            unselectTag={this.unselectTag}
+          />
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default GallerySection;
